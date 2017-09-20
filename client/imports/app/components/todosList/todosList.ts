3ab5b3c4ea6bee13a46c935b57439606646a4f38
Carlsiry <chenkansheng0@163.com>
2017-09-20 19:54:24 +0800

    添加任务删除和完成功能

--- client/imports/app/components/todosList/todosList.ts
+++ client/imports/app/components/todosList/todosList.ts
@@ -16,15 +16,41 @@ import { Task } from '../../../../../imports/models/collection-object.model';
 export class TodosListComponent implements OnInit{
     tasks: Observable<Task[]>; // 组件属性：所有的任务为可观察流
     constructor() {
-        this.tasks = Tasks.find({}).zone(); // 组件实例时，查询所有的任务
+        this.tasks = Tasks.find({}, {
+            sort: {
+                createdAt: -1 // 文档按时间从大到小排序
+            }
+        }).zone(); // 组件实例化时，查询所有的任务
     }
     ngOnInit() {
     }
     // 组件方法：用于增加任务到数据模型文档集中
-    addTask(newTask: string) {
-        console.log(newTask);
+    addTask(newTask: any) {
+        console.log(JSON.stringify(newTask)); // ??? 
+        // {"__zone_symbol__keyupfalse":[
+            // {"type":"eventTask","state":"running",
+            // "source":"HTMLInputElement.addEventListener:keyup",
+            // "zone":"<root>","runCount":34}
+        // ]}
         Tasks.insert({
-            text: newTask
+            text: newTask.value,
+            checked: false,
+            createdAt: new Date()
+        });
+        // 重置表单
+        newTask.value = '';
+    }
+    // 删除任务
+    removeTask(task: any) {
+        console.log(JSON.stringify(task))
+        Tasks.remove(task._id);
+    }
+    // 设置任务是否完成
+    setChecked(task: Task) {
+        Tasks.update(task._id, {
+            $set: {
+                checked: !task.checked
+            }
         });
     }
 }
--- imports/models/collection-object.model.ts
+++ imports/models/collection-object.model.ts
@@ -4,6 +4,9 @@ export interface CollectionObject {
     _id?: string;
 }
 
+// 任务文档的数据结构类型
 export interface Task extends CollectionObject {
-    text: string
+    text: string, // 描述任务的文本
+    checked: boolean, // 是否完成
+    createdAt: Date // 任务创建的时间 
 }
